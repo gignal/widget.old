493b5b157dc29e5c744f15b2b3bac5a27d7da30d
Dalia <dalia@gignal.com>
2013-03-06 20:02:38 +0000

    first commit

--- javascript/models.js
+++ javascript/models.js
@@ -3,9 +3,7 @@ app.models.Event = Backbone.Model.extend({
 	//urlRoot: 'http://api.gignal.com/event/api/eventId/', // CloudFront
 	//urlRoot: 'http://gignal.com/event/api/eventId/', // direct on production server
 	urlRoot: 'http://dev.gignal.com/event/api/eventId/', // direct on dev server
-
 	idAttribute: 'event_id'
-	
 });
 
 
@@ -17,3 +15,4 @@ app.models.Text = Backbone.Model.extend({
 app.models.Photo = Backbone.Model.extend({
 	idAttribute: 'photo_stream_id'
 });
+  
